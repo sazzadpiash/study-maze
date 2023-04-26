d46fc8aaa2430a88d2f0f585962c3e9905d598a1
Sazzad Hossain <piash@Pishu.local>
2023-04-26 22:54:34 +0600

    last commit for today

---
+++ src/layout/layout.tsx
@@ -0,0 +1,12 @@
+import Header from "@/components/Header/Header";
+
+const Layout = ({children}) => {
+    return (
+        <div>
+            <Header></Header>
+            {children}
+        </div>
+    );
+};
+
+export default Layout;
