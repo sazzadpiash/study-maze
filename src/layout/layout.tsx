97316ebafebf941d94d158b92d8071ff686d5e54
Sazzad Hossain <piash@Pishu.local>
2023-04-26 23:01:05 +0600

    last commit for today

--- src/layout/layout.tsx
+++ src/layout/layout.tsx
@@ -1,10 +1,12 @@
+import Footer from "@/components/Footer/Footer";
 import Header from "@/components/Header/Header";
 
-const Layout = ({children}) => {
+const Layout = ({children}:any) => {
     return (
         <div>
             <Header></Header>
             {children}
+            <Footer></Footer>
         </div>
     );
 };
