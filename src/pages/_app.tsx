185e4ee8dd8ee7a450a07dfa256184a98a3c4eaf
pulok-thedeveloper <mdpulokhasan@gmail.com>
2023-05-01 19:49:38 +0600

    firebase setup and context added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,7 @@ import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
   return <div>
+    
     <Layout><Component {...pageProps} /></Layout>
   </div>
 }
