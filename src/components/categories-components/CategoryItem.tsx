53c74169a4df69ae59dcf7391cc109f9ae2a4062
Sazzad Hossain <piash@Pishu.local>
2023-05-03 15:58:53 +0600

    upload server data, fetch data

--- src/components/categories-components/CategoryItem.tsx
+++ src/components/categories-components/CategoryItem.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import { useContext } from "react";
 
 
-const CategoryItem = ({ category, location }: any) => {
+const CategoryItem = ({icon, category, location }: any) => {
     // const { products } : any = useContext();
     // const availableProducts = products.filter(product => product?.inStock === "available")
     // const count = availableProducts?.filter(product => product?.categoryId === category?._id)
@@ -13,19 +13,19 @@ const CategoryItem = ({ category, location }: any) => {
             <Link href={``}>
                 <button className="hover:text-[#f95c39]">
                     <div className="flex justify-start items-center">
-                        {/* <div className="mx-1">
-                            <img src={category?.icon} alt={category?.icon} className="w-4 h-4" />
-                        </div> */}
+                        <div className="mx-1">
+                            <img src={icon} alt={icon} className="w-4 h-4" />
+                        </div>
                         <div className="flex text-sm">
                             <p className="text-left">
-                                {category.length < 20 ? category : category.slice(0, 19)}
+                                {category.length < 30 ? category : category.slice(0, 19)}
                                 {
-                                    category.length < 25 ? "" : "..."
+                                    category.length < 30 ? "" : "..."
                                 }
                             </p>
-                            <p className="mx-4 text-gray-500">
+                            {/* <p className="mx-4 text-gray-500">
                                 (1000)
-                            </p>
+                            </p> */}
                         </div>
                     </div>
                 </button>
