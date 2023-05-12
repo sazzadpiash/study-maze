d6d4619b337b929bd10e67aa0d4af6320434bf4b
Hussain <iamhussainali2@gmail.com>
2023-05-12 07:37:16 +0530

    fixed some more issue

--- next.config.js
+++ next.config.js
@@ -2,7 +2,8 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-    output: 'export',
+    // output: 'export',
+    start:"next start"
     // Optional: Add a trailing slash to all paths `/about` -> `/about/`
     // trailingSlash: true,
     // Optional: Change the output directory `out` -> `dist`
